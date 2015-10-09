4ca4ed2875760b0b9383786b9904e5b703d7fdcd
ibeerepoot <ibeerepoot@hotmail.com>
2015-10-09 22:35:59 +0200

    Solved the colouring problems

--- Homework/SVG3/main3.js
+++ Homework/SVG3/main3.js
@@ -1,6 +1,6 @@
 /* use this to test out your function */
 window.onload = function() {
- 	parseData();
+ 	colorMap();
 }
 
 /* changeColor takes a path ID and a color (hex value)
@@ -9,7 +9,7 @@ function changeColor(id, color) {
     document.getElementById(id).style.fill=color;
 }
 
-function parseData() {
+function colorMap() {
 	// get the JSON data from the DOM
 	var el = document.getElementById("dataset").innerHTML;
 	// parse JSON to Javascript object
@@ -19,7 +19,7 @@ function parseData() {
 	var popValues = [];
 
 	// keep track of which countries in parsed are EU countries
-	var euCountries = []
+	var euCountries = [];
 
 	// loop through all countries in the dataset
 	for (key in parsed) {
@@ -41,7 +41,7 @@ function parseData() {
 	var range = maxValue - minValue;
 
 	// fill in the colour spectrum we will use
-	var spectrum = ["#166a62", "#276f63", "#387464", "#497a64", "#5a7f65", "#6b8466", "#7d8a67", "#8e8f67", "#9f9468", "#b09969", "#c19e6a", "#d2a46a", "#e3a96b"];
+	var spectrum = ["#092b27", "#183934", "#32524d", "#4f6e6a", "#65847f", "#71908b", "#7d9c97", "#8baaa6", "#9ab9b4", "#a9c7c3", "#bad8d4", "#cceae6", "#e0fefa"];
 	
 	// calculate the amount that will be in one colour
 	var numColours = spectrum.length;
