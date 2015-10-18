c17630619f8b4506eecb3557bb3dfb8d86bcc6d4
ibeerepoot <ibeerepoot@hotmail.com>
2015-10-18 14:31:51 +0200

    Aan het aanmodderen met interactiviteit in D3

--- Homework/D3/d3map/d3map.js
+++ Homework/D3/d3map/d3map.js
@@ -54,8 +54,8 @@ function mapData() {
 		geographyConfig: {
             popupTemplate: function(geo, data) {
                 return ['<div class="hoverinfo"><strong>',
-                        geo.properties.name + '<br>',
-                        'Population: ' + data.population,
+                        'Population density in '+ geo.properties.name,
+                        ': ' + data.population,
                         '</strong></div>'].join('');
             }
         },
@@ -64,17 +64,68 @@ function mapData() {
 	// add country to the data property in the datamap object
 	for (var country in parsed) {
 		var codeOfCountry = parsed[key].code;
-		map.options.data[codeOfCountry] = { fillKey: "", population: "100"};
+		map.options.data[codeOfCountry] = { fillKey: "", population: ""};
 	}
 
 	console.log(map.options.data);
 	
-	var group = "groep1";
-	
+	var fillKey_value = "groep1";
+	var population_value = "100";
+
+	// create array with all density values
+	var density_values = [];
+	for (var values in parsed) {
+		console.log(parsed[values].value); 
+		density_values.push(parsed[values].value);
+	}
+	console.log(density_values);
+
+	/*
+	min = 0.02, max = 19416
+	meeste onder de 100, dan nog een paar tot 500
+	weinig boven de 500
+
+	1. 5 0-5
+	2. 10 5-15
+	3. 20 15-35
+	4. 40 35-75
+	5. 80 75-155
+	6. 160 155-315
+	7. 320 315-635
+	8. 640 635-1275
+	9. 1280 1275-2555
+	10. 2560 2555 - 5115
+	11. 5120 5115 - 10235
+	12. 10240 10235 - 20475
+	13.
+
+	1. 1 > 0-1
+	2. 2 > 1-3
+	3. 4 > 3-7
+	4. 8 > 7-15
+	5. 16 > 15-31
+	6. 32 > 31-63
+	7. 64 > 63-127
+	8. 128 > 127-255
+	9. 256 > 255-511
+	10. 512 > 511-1023
+	11. 1024 > 1023-2047
+	12. 2048 > 2047-4095
+	13. 4096 > 4095-max
+	*/
+
 	// loop through all country keys in map.data
 	for (key in map.options.data) {
+		console.log(key);
 		var data = {};
-		data[key] = { fillKey: group};
-		map.updateChoropleth(data);
+		// figure out the right population value
+		for (countries in parsed) {
+			if (parsed[countries].code == key);
+			population_value = parsed[countries].value;
+		}
+		// change the fillkey and population properties to the right value
+		data[key] = { fillKey: fillKey_value, population: population_value};
+		
 	}
+	map.updateChoropleth(data);
 }
